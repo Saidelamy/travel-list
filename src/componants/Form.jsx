1980c27a85eab62fd06a88911ed89392c50c8630
said magdy <saidmagdy511@gmail.com>
2025-02-17 21:11:47 +0200

    .

--- src/componants/Form.jsx
+++ src/componants/Form.jsx
@@ -5,26 +5,26 @@ export default function Form({ handleAddItem }) {
   const [quantity, setQuantity] = useState(1);
 
   function handleAdd(e) {
+    // const newItem = { description, quantity, id: Date.now(), packed: false };
+
     e.preventDefault();
-
     if (!description) return;
-    const newItem = { description, quantity, id: Date.now(), packed: false };
-    console.log(newItem);
-
+    const newItem = { quantity, description, id: Date.now(), packed: false };
     handleAddItem(newItem);
-
     setDescription("");
     setQuantity(1);
   }
+
   return (
     <>
       <form className="add-form" onSubmit={handleAdd}>
         <h3>what do you need for your 🛒 trip ?</h3>
+
         <select
           value={quantity}
           onChange={(e) => setQuantity(Number(e.target.value))}
         >
-          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
             <option value={num} key={num}>
               {num}
             </option>
